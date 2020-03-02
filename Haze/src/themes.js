0e91915753b70fde300b86ef97b72d82b1180834
ninryu4 <16454061+ninryu@users.noreply.github.com>
2020-03-02 17:00:56 +0200

    Delete themes.js

--- Haze/src/themes.js
+++
@@ -1,9 +0,0 @@
-function changeBackground(){
-    var randomNumber = Math.floor(Math.random() * 1) + 1;
-    
-    if (randomNumber === 1){
-    document.getElementById('divimagen').style.backgroundColor = "#ff6666";
-    document.getElementById('divimagen').style.backgroundImage = "url(src/imagenes/1.png)";
-
-}
-}
